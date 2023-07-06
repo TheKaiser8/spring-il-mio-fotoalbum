b7a0c209784507efb98d35186b5463c0063dba91
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-07-06 11:03:55 +0200

    Add Spring project scaffolding

---
+++ src/main/java/org/lessons/springmyphotogallery/SpringMyPhotoGalleryApplication.java
@@ -0,0 +1,13 @@
+package org.lessons.springmyphotogallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyPhotoGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyPhotoGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lessons/springmyphotogallery/SpringMyPhotoGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.springmyphotogallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMyPhotoGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
