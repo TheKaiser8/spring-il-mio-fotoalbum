2afa450faabda8baf93534ea3c094d8ab3be1068
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-07-07 12:00:26 +0200

    Add photo edit form page with controller methods (edit & update) - add button to edit page

--- src/main/java/org/lessons/springmyphotogallery/controller/PhotoController.java
+++ src/main/java/org/lessons/springmyphotogallery/controller/PhotoController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.lessons.springmyphotogallery.messages.AlertMessage;
 import org.lessons.springmyphotogallery.messages.AlertMessageType;
 import org.lessons.springmyphotogallery.model.Photo;
+import org.lessons.springmyphotogallery.repository.PhotoRepository;
 import org.lessons.springmyphotogallery.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,6 +18,9 @@ import java.util.List;
 @Controller
 @RequestMapping("/photos")
 public class PhotoController {
+
+    @Autowired
+    private PhotoRepository photoRepository;
 
     @Autowired
     PhotoService photoService;
@@ -60,6 +64,31 @@ public class PhotoController {
         // aggiungo un messaggio di successo come flash attribute utilizzando un classe CUSTOM per personalizzare i messaggi degli alert
         redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "La foto " + "\"" + formPhoto.getTitle() + "\"" + " è stata creata!"));
         // se l'operazione va a buon fine rimando alla lista delle foto
+        return "redirect:/photos";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable("id") Integer photoId, Model model) {
+        Photo photo = photoService.getById(photoId);
+        model.addAttribute("photo", photo);
+        return "photos/form";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String update(@PathVariable("id") Integer photoId, @Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        // cerco per id la versione vecchia della pizza da modificare
+        Photo photoToEdit = photoService.getById(photoId);
+        // se ci sono stati errori restituisco il form con i campi precompilati
+        if (bindingResult.hasErrors()) {
+            return "photos/form";
+        }
+        // trasferisco su formPhoto tutti i valori dei campi che non sono presenti o non sono modificabili nel form (altrimenti vengono persi)
+        formPhoto.setId(photoToEdit.getId());
+        formPhoto.setCreatedAt(photoToEdit.getCreatedAt());
+        // salvo la foto aggiornata
+        photoRepository.save(formPhoto);
+        // aggiungo un messaggio di successo come flash attribute utilizzando un classe CUSTOM per personalizzare i messaggi degli alert
+        redirectAttributes.addFlashAttribute("message", new AlertMessage(AlertMessageType.SUCCESS, "La foto " + "\"" + formPhoto.getTitle() + "\"" + " è stata aggiornata!"));
         return "redirect:/photos";
     }
 }
