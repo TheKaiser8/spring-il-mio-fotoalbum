dd8941a5841025817b8093989f710587506886e4
TheKaiser8 <stefano.vanzini8@gmail.com>
2023-07-07 09:18:21 +0200

    Add photo show page

--- src/main/java/org/lessons/springmyphotogallery/controller/PhotoController.java
+++ src/main/java/org/lessons/springmyphotogallery/controller/PhotoController.java
@@ -6,11 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/photos")
@@ -22,10 +22,18 @@ public class PhotoController {
     // READ METHODS
     @GetMapping
     public String index(@RequestParam(name = "keyword", required = false) String searchString, Model model) {
-        List<Photo> photos = photoService.getAllWithOptParam(searchString == null ? Optional.empty() : Optional.of(searchString));
+        List<Photo> photos = photoService.getAllWithOptParam(searchString);
         model.addAttribute("photoList", photos);
         // aggiungo un altro attributo al model per mantenere il valore della input dopo l'invio della ricerca filtrata
         model.addAttribute("searchInput", searchString);
         return "photos/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer photoId, Model model) {
+        Photo photo = photoService.getById(photoId);
+        model.addAttribute("photo", photo);
+        return "photos/show";
+    }
+    
 }
--- src/main/java/org/lessons/springmyphotogallery/service/PhotoService.java
+++ src/main/java/org/lessons/springmyphotogallery/service/PhotoService.java
@@ -3,7 +3,9 @@ package org.lessons.springmyphotogallery.service;
 import org.lessons.springmyphotogallery.model.Photo;
 import org.lessons.springmyphotogallery.repository.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,11 +17,24 @@ public class PhotoService {
     PhotoRepository photoRepository;
 
     // metodo che restituisce la lista foto filtrata o no a seconda del parametro OPZIONALE
-    public List<Photo> getAllWithOptParam(Optional<String> keywordOpt) {
+    public List<Photo> getAllWithOptParam(String searchString) {
+        Optional<String> keywordOpt = Optional.ofNullable(searchString); // se il parametro è nullo ritorno un Optional object vuoto
         if (keywordOpt.isEmpty()) {
             return photoRepository.findAll();
         } else {
             return photoRepository.findByTitleContainingIgnoreCase(keywordOpt.get());
         }
     }
+
+    // metodo che restituisce, se presente, la foto presa per id oppure lancia un'eccezione
+    public Photo getById(Integer id) throws ResponseStatusException {
+        Optional<Photo> photoOpt = photoRepository.findById(id);
+        if (photoOpt.isPresent()) {
+            return photoOpt.get();
+        } else {
+            // ritorno un HTTP Status 404 Not Found
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "La foto con ID " + id + " non è stata trovata");
+        }
+    }
+
 }
